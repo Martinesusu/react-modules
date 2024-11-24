090c3bd0d28de87226dfd6af206c83ce72fba6d8
Martinesusu <tinnaphop.t@st.econ.tu.ac.th>
2024-11-25 00:35:06 +0700

    edit react-modules

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@chakra-ui/react', '@emotion/react', '@emotion/styled', 'framer-motion'],
+  },
   test: {
     globals: true,
     environment: "jsdom",
